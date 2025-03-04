bd0d0baba11945f0610dd0ac2a1bf4b7bc0ee92c
famgz <famgz@proton.me>
2025-03-04 19:44:54 -0300

    chore: initial setup

---
+++ NOTAS.md
@@ -0,0 +1,11 @@
+`npm create vite@latest .`
+
+`npm install tailwindcss @tailwindcss/vite`
+
+`npm install -D @types/node`
+
+`npx shadcn@latest init`
+
+`npx shadcn@latest add button`
+
+`npm i zustand`
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello World</h1>
+      <Button>click me</Button>
+    </div>
+  );
+}
