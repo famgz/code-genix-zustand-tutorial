40dc437ea64ae1c1993210ca94b482546e186b48
famgz <famgz@proton.me>
2025-03-05 00:20:46 -0300

    feat: persist data

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,16 +2,19 @@ import { createCartSlice } from '@/store/cart-slice';
 import { createUserSlice } from '@/store/user-slice';
 import { Store } from '@/types/store';
 import { create } from 'zustand';
-import { devtools, subscribeWithSelector } from 'zustand/middleware';
+import { devtools, persist, subscribeWithSelector } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 export const useStore = create<Store>()(
   devtools(
     subscribeWithSelector(
-      immer((...x) => ({
-        ...createUserSlice(...x),
-        ...createCartSlice(...x),
-      }))
+      persist(
+        immer((...x) => ({
+          ...createUserSlice(...x),
+          ...createCartSlice(...x),
+        })),
+        { name: 'zustand-exercise' }
+      )
     )
   )
 );
