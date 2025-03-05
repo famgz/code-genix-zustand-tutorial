d7405585bc836282bbc89f237076b21441eeb541
famgz <famgz@proton.me>
2025-03-04 23:47:15 -0300

    feat: add zustand devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,14 @@ import { createCartSlice } from '@/store/cart-slice';
 import { createUserSlice } from '@/store/user-slice';
 import { Store } from '@/types/store';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 export const useStore = create<Store>()(
-  immer((...x) => ({
-    ...createUserSlice(...x),
-    ...createCartSlice(...x),
-  }))
+  devtools(
+    immer((...x) => ({
+      ...createUserSlice(...x),
+      ...createCartSlice(...x),
+    }))
+  )
 );
