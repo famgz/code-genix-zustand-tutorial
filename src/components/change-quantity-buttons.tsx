816e58781231808cfb529d4b42f120b9d2b1664f
famgz <famgz@proton.me>
2025-03-05 00:05:35 -0300

    feat: ensure cart total will be updated

--- src/components/change-quantity-buttons.tsx
+++ src/components/change-quantity-buttons.tsx
@@ -2,20 +2,36 @@ import { Button } from '@/components/ui/button';
 import { useStore } from '@/store/store';
 import { useShallow } from 'zustand/react/shallow';
 import { MinusIcon, PlusIcon } from 'lucide-react';
+import { useEffect } from 'react';
 
 interface Props {
   productId: string;
 }
 
 export default function ChangeQuantityButtons({ productId }: Props) {
-  const { getProductById, decrementQuantity, incrementQuantity } = useStore(
-    useShallow((state) => ({
-      getProductById: state.getProductById,
-      decrementQuantity: state.decrementQuantity,
-      incrementQuantity: state.incrementQuantity,
-    }))
-  );
+  const { getProductById, decrementQuantity, incrementQuantity, setTotal } =
+    useStore(
+      useShallow((state) => ({
+        getProductById: state.getProductById,
+        decrementQuantity: state.decrementQuantity,
+        incrementQuantity: state.incrementQuantity,
+        setTotal: state.setTotal,
+      }))
+    );
   const product = getProductById(productId);
+
+  useEffect(() => {
+    const unSub = useStore.subscribe(
+      (state) => state.products,
+      (products) => {
+        setTotal(
+          products.reduce((acc, item) => acc + item.price * item.quantity, 0)
+        );
+      },
+      { fireImmediately: true }
+    );
+    return unSub;
+  }, [setTotal]);
 
   return (
     <>
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,16 @@ import { createCartSlice } from '@/store/cart-slice';
 import { createUserSlice } from '@/store/user-slice';
 import { Store } from '@/types/store';
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, subscribeWithSelector } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 export const useStore = create<Store>()(
   devtools(
-    immer((...x) => ({
-      ...createUserSlice(...x),
-      ...createCartSlice(...x),
-    }))
+    subscribeWithSelector(
+      immer((...x) => ({
+        ...createUserSlice(...x),
+        ...createCartSlice(...x),
+      }))
+    )
   )
 );
